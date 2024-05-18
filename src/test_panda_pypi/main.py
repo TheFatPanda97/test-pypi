3ab666267d8d671b778be4d42c8bd7b73b2dd678
TheFatPanda97 <shawn.hu@laplacerobotics.com>
2024-05-18 02:42:04 +0000

    bumped version

---
+++ src/test_panda_pypi/main.py
@@ -0,0 +1,12 @@
+from .app import add_int
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Add two integers.')
+    parser.add_argument('num1', type=int, help='First integer')
+    parser.add_argument('num2', type=int, help='Second integer')
+    args = parser.parse_args()
+
+    result = add_int(args.num1, args.num2)
+    print(f"The sum of {args.num1} and {args.num2} is {result}")
